fde4636898042bf6634c65b6b2cb5bc5be1c93bb
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-08-30 15:25:04 -0300

    Aprendendo mais sobre React

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,7 +1,7 @@
 export default ({ socialNetwork, href }) => {
   return (
     <a
-      className="font-bold bg-[#428CD1] bottom-10 rounded-2xl w-36 py-3 text-center cursor-pointer text-white"
+      className="font-bold bg-[#428CD1] bottom-10 rounded-2xl w-36 py-3 text-center cursor-pointer text-white hover:bg-cyan-500"
       href={href}
     >
       {socialNetwork}
--- src/Components/NameProfile/NameProfile.jsx
+++ src/Components/NameProfile/NameProfile.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default ({ nameProfile }) => {
+  const [followText, setFollowText] = useState("Follow");
+
+  function handleClick() {
+    alert("Você agora está seguindo");
+    setFollowText("Following");
+  }
   return (
-    <>
+    <div className="flex pr-5 py-2 border-b-2 border-blue-500">
       <h2 className="text-black font-bold text-2xl text-center">
         {nameProfile}
       </h2>
-      <hr className=" border-sky-500 border-t-2" />
-    </>
+      <button
+        onClick={handleClick}
+        className=" w-24 bg-[#428CD1] rounded-2xl text-center cursor-pointer  text-white hover:bg-cyan-500"
+      >
+        {followText}
+      </button>
+      <br />
+    </div>
   );
 };
