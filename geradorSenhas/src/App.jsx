5f9719c4819a95c7d5f7a4dc266f829347e9e36e
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-09-02 14:28:09 -0300

    Trabalhando com inputs

--- geradorSenhas/src/App.jsx
+++ geradorSenhas/src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Button from "./Components/Button/Button";
 import randomPassword from "./hooks/randomPassword";
 import setCopyText from "./hooks/setCopyText";
 
 function App() {
-  const { randomNumber, password } = randomPassword();
+  const { randomNumber, password, passWordSize, setPasswordSize } =
+    randomPassword();
   const { copyText, copyToClipboard } = setCopyText();
+
   return (
     <div className="flex flex-col items-center gap-4 pt-52">
       <h1 className="text-white text-5xl">Gerador de Senhas</h1>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="passwordSize" className="text-white font-bold">
+          Tamanho: {passWordSize} Caracteres
+        </label>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passWordSize}
+          onChange={(ev) => {
+            setPasswordSize(ev.target.value);
+          }}
+          className="rounded-lg py-1 border-4 border-white bg-transparent text-white text-center"
+        />
+      </div>
       <div className="flex gap-3">
         <Button onClick={randomNumber} nameButton={"Gerar!"} />
         <Button
--- geradorSenhas/src/hooks/randomPassword.js
+++ geradorSenhas/src/hooks/randomPassword.js
@@ -2,19 +2,18 @@ import { useState } from "react";
 
 export default () => {
   const [password, setPassword] = useState("");
-
+  const [passWordSize, setPasswordSize] = useState(6);
   const randomNumber = () => {
     const characters =
       "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
-    const length = 12;
     let newPassword = "";
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passWordSize; i++) {
       const position = Math.floor(Math.random() * characters.length);
       newPassword += characters[position];
     }
     setPassword(newPassword);
   };
-  return { randomNumber, password };
+  return { randomNumber, password, passWordSize, setPasswordSize };
 };
 
 // const number = Math.floor(Math.random() * (max - min + 1) + min);
